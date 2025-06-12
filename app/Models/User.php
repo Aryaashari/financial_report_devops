700d1636c656a7aa91ed0a06243e60c12fe20723
MNFI13-blode <naufaltaurus24@gmail.com>
2025-06-13 01:18:53 +0700

    Update output

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,12 +3,11 @@
 namespace App\Models;
 
 use App\Notifications\CustomVerifyEmail;
-use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-class User extends Authenticatable implements MustVerifyEmail
+class User extends Authenticatable
 {
     use HasFactory, Notifiable;
 
@@ -44,9 +43,4 @@ class User extends Authenticatable implements MustVerifyEmail
         return $this->hasMany(Transaction::class);
     }
 
-    // 📨 Custom Email Verification
-    public function sendEmailVerificationNotification()
-    {
-        $this->notify(new CustomVerifyEmail);
-    }
 }
